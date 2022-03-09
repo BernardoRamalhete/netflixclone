4b8e72338326dede6c454b6ce88922241724393e
Bernardo Ramalhete <Bernardo>
2022-03-09 18:21:17 -0300

    changed a broken import in ProfileAdd.js

--- frontend/src/components/ProfileAdd.js
+++ frontend/src/components/ProfileAdd.js
@@ -1,5 +1,5 @@
 import '../styles/ProfileAdd.css'
-import user1 from '../../build/media/user1.png'
+import user1 from '../images/user1.png'
 import user2 from '../images/user2.png'
 import user3 from '../images/user3.png'
 import {Link} from 'react-router-dom'
